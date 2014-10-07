b574e0b3439b55adfe4bb57a5cddd091ca076886
Jamffy <l836828946@gmail.com>
2014-10-07 19:36:49 +0800

    complete Pull_Down_Menu.

--- Pull_Down_Menu/src/jamffy/example/pull_down_menu/MainActivity.java
+++ Pull_Down_Menu/src/jamffy/example/pull_down_menu/MainActivity.java
@@ -41,21 +41,52 @@ public class MainActivity extends Activity {
 		for (int i = 0; i < 20; i++) {
 			mList.add(100000 + i);
 		}
-
+		initListView();
 		wIv_down.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// 创建popwindow
-				wPop = new PopupWindow();
+				// 点击下拉菜单后，如果不存在则弹出popwindow
+				if (wPop == null) {
+					wPop = new PopupWindow();
+				}
 
-				wLv = new ListView(MainActivity.this);
-				wLv.setAdapter(new MyAdapter());
-				
+				if (wPop.isShowing()) {
+					// 如果存在则销毁
+					wPop.dismiss();
+				} else {
+					initPopWindow();
+				}
 
 			}
+
 		});
 
+	}
+
+	private void initListView() {
+		wLv = new ListView(MainActivity.this);
+		wLv.setBackgroundResource(R.drawable.listview_background);
+		// wLv.setDivider(null); // 设置条目之间的分隔线
+		wLv.setVerticalScrollBarEnabled(false);
+		wLv.setAdapter(new MyAdapter());
+
+	}
+
+	// 创建popwindow
+	private void initPopWindow() {
+
+		// 设置宽高
+		// int height=getWindowManager().getDefaultDisplay().getHeight();
+		wPop.setHeight(400);
+		// 与输入框同宽
+		wPop.setWidth(wEt.getWidth());
+
+		// 设置pop的内容
+		wPop.setContentView(wLv);
+		// 设置pop的位置
+		wPop.showAsDropDown(wEt, 0, 0);
+		wPop.setOutsideTouchable(true); // 点击popWin 以处的区域，自动关闭 popWin
 	}
 
 	public class MyAdapter extends BaseAdapter {
@@ -77,12 +108,13 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		public View getView(final int position, View convertView,
+				ViewGroup parent) {
 			View view = null;
 			ViewHolder holder = null;
 			if (convertView != null) {
 				holder = (ViewHolder) convertView.getTag();
-
+				view = convertView;
 			} else {
 				view = View.inflate(getApplicationContext(), R.layout.list,
 						null);
@@ -98,6 +130,19 @@ public class MainActivity extends Activity {
 				@Override
 				public void onClick(View v) {
 					// 点击删除后，从集合中删除数据，并通知listview更新
+					mList.remove(position);
+					
+					MyAdapter.this.notifyDataSetChanged();
+				}
+			});
+
+			view.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// 点击条目后，将内容输出到editview中
+					wEt.setText(mList.get(position) + "");
+					wPop.dismiss();
 
 				}
 			});
