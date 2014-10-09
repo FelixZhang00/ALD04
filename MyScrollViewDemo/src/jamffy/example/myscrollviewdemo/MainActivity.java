92f1636d1937c463be2250ecf82ea02125236c29
Jamffy <l836828946@gmail.com>
2014-10-09 11:23:44 +0800

    commit

--- MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MainActivity.java
+++ MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MainActivity.java
@@ -4,31 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	private MyScrollView wSv;
+	private int[] imageId={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		wSv=(MyScrollView) findViewById(R.id.myscrollview);
+		
+		for(int i=0;i<imageId.length;i++){
+			ImageView iv=new ImageView(this);
+			iv.setBackgroundResource(imageId[i]);
+			wSv.addView(iv);
 		}
-		return super.onOptionsItemSelected(item);
 	}
+
 }
---
+++ MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MyScrollView.java
@@ -0,0 +1,26 @@
+package jamffy.example.myscrollviewdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyScrollView extends ViewGroup {
+
+	public MyScrollView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
