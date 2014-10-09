1604d586caeb355e3e5beb5dd4410cbda7c5c54b
Jamffy <l836828946@gmail.com>
2014-10-09 14:43:31 +0800

    commit

--- MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MainActivity.java
+++ MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MainActivity.java
@@ -9,16 +9,20 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
 
 	private MyScrollView wSv;
-	private int[] imageId={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f};
+	private int[] imageId = { R.drawable.a, R.drawable.b, R.drawable.c,
+			R.drawable.d, R.drawable.e, R.drawable.f };
+
+	// private int[] imageId={R.drawable.ic_launcher};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		wSv=(MyScrollView) findViewById(R.id.myscrollview);
-		
-		for(int i=0;i<imageId.length;i++){
-			ImageView iv=new ImageView(this);
+
+		wSv = (MyScrollView) findViewById(R.id.myscrollview);
+
+		for (int i = 0; i < imageId.length; i++) {
+			ImageView iv = new ImageView(this);
 			iv.setBackgroundResource(imageId[i]);
 			wSv.addView(iv);
 		}
--- MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MyScrollView.java
+++ MyScrollViewDemo/src/jamffy/example/myscrollviewdemo/MyScrollView.java
@@ -2,25 +2,114 @@ package jamffy.example.myscrollviewdemo;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MyScrollView extends ViewGroup {
 
+	// 手势识别的工具类
+	private GestureDetector detector;
+	private Context context;
+
+	/**
+	 * 在代码里面创建对象的时候，使用此构造方法
+	 */
 	public MyScrollView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		this.context = context;
 	}
 
 	public MyScrollView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		this.context = context;
+		initView();
 	}
 
 	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		super.onTouchEvent(event);
+		System.out.println(" onTouch");
+		detector.onTouchEvent(event);
+
+		// onTounch 需要消费掉事件
+		return true;
+	}
+
+	/**
+	 * 初始化view
+	 */
+	private void initView() {
+		detector = new GestureDetector(context, new OnGestureListener() {
+
+			@Override
+			public boolean onSingleTapUp(MotionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("onSingleTapUp");
+				return false;
+			}
+
+			@Override
+			public void onShowPress(MotionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			/**
+			 * 响应手指在屏幕上的滑动事件
+			 */
+			public boolean onScroll(MotionEvent e1, MotionEvent e2,
+					float distanceX, float distanceY) {
+
+				System.out.println("移动屏幕：" + distanceX);
+				scrollBy((int) distanceX, 0);
+
+				return false;
+			}
+
+			@Override
+			public void onLongPress(MotionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			// 快速滑动时调用
+			public boolean onFling(MotionEvent e1, MotionEvent e2,
+					float velocityX, float velocityY) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean onDown(MotionEvent e) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+
+	}
+
+	@Override
+	/**
+	 * 对子view进行布局，确定子view的位置
+	 * changed  若为true ，说明布局发生了变化
+	 * l\t\r\b\  是指当前viewgroup 在其父view中的位置 
+	 */
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < getChildCount(); i++) {
+			View view = getChildAt(i);
+			/**
+			 * 父view 会根据子view的需求，和自身的情况，来综合确定子view的位置,(确定他的大小)
+			 */
+			// 指定子view的位置 , 左，上，右，下，是指在viewGround坐标系中的位置
+			view.layout(0 + i * getWidth(), 0, getWidth() + i * getWidth(),
+					getHeight());
+		}
+
 	}
 
 }
