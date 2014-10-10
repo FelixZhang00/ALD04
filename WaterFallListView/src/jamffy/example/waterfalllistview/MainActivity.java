f87a6157fc4e42d8cd22bd8c12bb91415e99b0ba
Jamffy <l836828946@gmail.com>
2014-10-10 15:13:16 +0800

    commit new project:WallFallListview

---
+++ WaterFallListView/src/jamffy/example/waterfalllistview/MainActivity.java
@@ -0,0 +1,20 @@
+package jamffy.example.waterfalllistview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
